09042e0bdd0145b825b90c5ba29d600ab8b4dd74
Raisa Bernardes <raisa.rmro@gmail.com>
2023-10-19 09:51:21 -0300

    navbar done

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews';
+import Popular from './Popular';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import Footer from './Footer';
+
+export {
+    Hero, 
+    CustomerReviews,
+    Popular,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    Footer
+}
