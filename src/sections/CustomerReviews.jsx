47eeeeec6f842c50b667390932f3a0dce241f8aa
Raisa Bernardes <raisa.rmro@gmail.com>
2023-10-24 21:03:07 -0300

    customer review image fixed

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,5 +1,5 @@
 import ReviewCard from "../components/ReviewCard";
-import { reviews } from '../constants';
+import { reviews } from "../constants";
 
 const CustomerReviews = () => {
   return (
@@ -11,7 +11,7 @@ const CustomerReviews = () => {
       </h3>
       <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stories from our satisfied customers about their exceptional experiences with us</p>
       <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
-        {reviews.map((index, review) => (
+        {reviews.map((review, index) => (
           <ReviewCard 
           key={index}
           imgURL={review.imgURL}
